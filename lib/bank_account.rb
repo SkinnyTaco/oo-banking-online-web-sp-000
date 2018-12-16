26231261a59d508729ca1c19dec96e509ac2e674
Daniel Dawson <dannyldawson@gmail.com>
2018-12-16 18:33:07 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,24 @@
 class BankAccount
+  
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit
+    
+  end
+  
+  def display_balance
+    @balance
+  end
+  
+  def valid?
+    @status.
+  end
 
 end
